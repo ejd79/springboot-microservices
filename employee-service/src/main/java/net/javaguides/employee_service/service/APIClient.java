92b3d1b674da89ed2897d7907d7a73095441b7ac
ejd79 <erickjd79@gmail.com>
2024-08-16 15:37:25 +0200

    Adding EUREKA SERVICE REGISTRY

--- employee-service/src/main/java/net/javaguides/employee_service/service/APIClient.java
+++ employee-service/src/main/java/net/javaguides/employee_service/service/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", name = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("api/departments/{department-code}")
