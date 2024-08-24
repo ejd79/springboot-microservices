603c74ad26440a3b4d55183d275799414da91f4b
ejd79 <erickjd79@gmail.com>
2024-08-24 18:34:41 +0200

    Adding ReactJS App

---
+++ react-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EmployeeComponent from './component/EmployeeComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <EmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
