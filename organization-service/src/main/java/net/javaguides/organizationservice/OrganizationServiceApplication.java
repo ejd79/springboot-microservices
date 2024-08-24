55ffb6a6a15cecb23c2bbeb4f3599c71a6ab554d
ejd79 <erickjd79@gmail.com>
2024-08-24 21:59:40 +0200

    Defining General and Customizing Model API Documentation

--- organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
@@ -1,10 +1,35 @@
 package net.javaguides.organizationservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Organization Service REST APIs",
+				description = "Organization Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Erick",
+						email = "erick@jeropache.co",
+						url = "javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "javaguides.net/licenses"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Organization Service Doc",
+				url = "javaguide.net/Organization_Service.html"
+		)
+)
 @SpringBootApplication
 public class OrganizationServiceApplication {
 
